6145c72c4b086064f7574fcf7bd4a6d544a5a1b9
Pawan1304 <pawan80069@gmail.com>
2026-09-18 00:00:43 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sudh-vaishno-tandoor-v6-motion';
+const CACHE = 'sudh-vaishno-tandoor-v7-motion';
 const APP_SHELL = [
   './', './index.html','./gallery.html','./story.html', './style.css', './app.js', './config.js', './supabase-config.js',
   './manifest.json', './favicon.png', './assets/photos/logo.png', './assets/icons/icon-192.png', './assets/icons/icon-512.png'
